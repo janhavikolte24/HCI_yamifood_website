49c5390b911a5438b1c057d0eb6423a06a962058
prasad-791 <aniketkhalkar791@gmail.com>
2021-11-17 13:07:23 +0530

    Menu Page

--- js_files/menu.js
+++ js_files/menu.js
@@ -1,5 +1,4 @@
 
-console.log('Hi');
 $('#myTab a').on('click', function (e) {
     e.preventDefault()
     $(this).tab('show')
@@ -12,13 +11,61 @@ $('#myTab a').on('click', function (e) {
     any.addClass('active');
 })
 
-// const tabC = document.getElementById('myTab');
-// const tabs = tabC.getElementsByClassName('nav-link');
-// for(var i=0;i<tabs.length;i++){
-//     tabs[i].addEventListener('click',function(){
-//         const current = tabC.getElementsByClassName('active');
-//         current[0].removeClass('active');
-//         // current[0].className = current[0].className.replace(" active", "");
-//         this.addClass('active');
-//     })
+cardContainer = document.getElementsByClassName('card-container');
+
+for(var i=0;i<cardContainer.length;i++){
+    cards = [].slice.call(cardContainer[i].getElementsByClassName('card'));
+    for(var i=0;i<cards.length;i++){
+        let addBtn = cards[i].getElementsByClassName('addBtn')[0];
+        addBtn.addEventListener("click",function(){
+            if(addBtn.innerHTML == '+'){
+                addBtn.innerHTML = '✓';
+            }else{
+                addBtn.innerHTML = '+'
+            }
+        });
+
+        let infoBtn = cards[i].getElementsByClassName('infoBtn')[0];
+        let img = cards[i].getElementsByClassName('card-img-top')[0];
+        let dishName = cards[i].getElementsByClassName('dishName')[0];
+        infoBtn.addEventListener("click",function(){
+            let mainPopUp = document.getElementById('ingredientPopUp');
+            mainPopUp.style.backgroundImage = `url(${img.src})`;
+            mainPopUp.getElementsByClassName('popUpDishName')[0].innerHTML = dishName.innerHTML;
+            var current = document.getElementById('nav-tabContent');
+            current.classList.add('blur');
+            mainPopUp.style.display = 'flex';
+        });
+    }
+}
+var isBillOpen = false;
+
+
+function closePopUp(){
+    document.getElementById('ingredientPopUp').style.display = 'none';
+    var current = document.getElementsByClassName('blur');
+    current[0].className = current[0].className.replace("blur", "");
+}
+
+function dragBill(){
+
+    if(isBillOpen){
+        document.getElementById("billdiv").style.transform = "translate(0%,100%)";
+        isBillOpen = false
+    }else{
+        document.getElementById("billdiv").style.transform = "translate(0%,0%)";
+        // document.getElementById("billdiv").style.transitionDuration = "4s";
+        isBillOpen = true;
+    }
+}
+
+
+
+
+// allAddBtns = document.getElementsByClassName('addBtn');
+// console.log(allAddBtns);
+// for(var i=0;i<allAddBtns.length;i++){
+//     allAddBtns[i].addEventListener("click",function(){
+//         console.log(allAddBtns[i].innerHTML);
+//     });
 // }
